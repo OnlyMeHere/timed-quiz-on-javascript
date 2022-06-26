43f38269e6e5a6b2a17b2de12edcd540abb1d8a3
OnlyMeHere <bennett45.j.m@gmail.com>
2022-06-26 16:10:14 -0700

    add CSS and JS

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# timed-quiz-on-javascript
+# Timed-Quiz-on-Javascript
+
+
+## Description:
+
+
+
+## Technologies Used:
+
+- ### HTML
+- ### CSS
+- ### JavaScript
+
+
+## Deployed Site Links:
+
+[Deployed Site](https://onlymehere.github.io/timed-quiz-on-javascript/)
+
+[github repository](https://github.com/OnlyMeHere/timed-quiz-on-javascript)
+
+
+## Code Snippets of Significance:
+This is important because...
+
+
+## Screen Shot & GIF of Site in Action:
+
+
+
+## Contact Me:
+
+
+
---
+++ assets/script.js
@@ -0,0 +1,28 @@
+var openTitle = document.querySelector("#openTitle");
+var timer = document.querySelector("#timer");
+var hiScore = document.querySelector("#hiScore");
+var startBtn = document.querySelector("#startBtn");
+var upBanner = document.querySelector("#upBanner");
+var choice1 = document.querySelector("#choice1");
+var choice2 = document.querySelector("#choice2");
+var choice3 = document.querySelector("#choice3");
+var choice4 = document.querySelector("#choice4");
+var lowBanner = document.querySelector("#lowBanner");
+
+
+startBtn.addEventListener("click", quiz());
+
+choice1.addEventListener("click"), 
+
+
+function quiz() {
+
+    console.log("Start Button Clicked");
+
+}
+
+function checkAnswer() {
+
+    console.log("Button 1 clicked");
+    if 
+}
